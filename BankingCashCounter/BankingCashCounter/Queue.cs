fc619c98f114bb19a9c8418cb7cb3b5bc0e80439
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-05 01:17:18 +0530

    Add Problem No.4

---
+++ BankingCashCounter/BankingCashCounter/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounter
+{
+    class Queue<T>
+    {
+        int capacity;
+        T[] obj;
+        int front = -1, rear = -1;
+        public Queue(int capacity)
+        {
+            this.capacity = capacity;
+            obj = new T[capacity];
+        }
+        public bool Enqueue(T data)
+        {
+            if (rear == capacity - 1)
+            {
+                Console.WriteLine("Que is Full");
+                return false;
+            }
+            if (rear == -1)
+                front++;
+            obj[++rear] = data;
+            return true;
+        }
+        public T Dequeue()
+        {
+            if (front == -1)
+            {
+                Console.WriteLine("que is empty");
+                return default(T);
+            }
+            T ob = obj[front++];
+            if (front > rear)
+                front = rear = -1;
+            return ob;
+        }
+        public bool IsEmpty()
+        {
+            if (rear == -1)
+                return true;
+            else return false;
+        }
+        public int Size()
+        {
+            if (rear == -1)
+            {
+                return 0;
+            }
+            return rear - front + 1;
+        }
+        public override string ToString()
+        {
+            String s = "";
+            for (int i = front; i <= rear && front != -1; i++)
+            {
+                s = s + obj[i] + " ";
+
+            }
+            return s;
+        }
+    }
+}
