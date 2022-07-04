ddc0be377d177ac962453b2e80f6d15bb582face
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-05 00:51:41 +0530

    Problem No. 1

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UnorderedList list = new UnorderedList();
+            list.addFirst("Vaibhav");
+            list.addFirst("Riya");
+            list.addFirst("Ajay");
+            list.addFirst("Priya");
+            list.addFirst("Mohit");
+            list.addFirst("Kajal");
+
+            Console.WriteLine("Enter Student Name:");
+            string input = Console.ReadLine();
+
+            bool check = list.search(input);
+
+            if (check == true)
+            {
+                // if input data is present in the list then then that data is removed from the list
+                list.remove(input);
+            }
+            else
+            {
+                //if input data is not present in the list then that data is added into the list
+                list.addFirst(input);
+            }
+
+            //Display the data present in the list after the completing the above  operations.
+            list.diplayData();
+        }
+    }
+}
