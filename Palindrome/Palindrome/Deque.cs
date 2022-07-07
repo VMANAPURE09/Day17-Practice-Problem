f0207b96b39124541ddbc8110fba7d885daf2566
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-08 00:10:57 +0530

    Add Palindrome Solution

---
+++ Palindrome/Palindrome/Deque.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Deque<T>
+    {
+        public class Node
+        {
+            public T data;
+            public Node pre;
+            public Node next;
+            public Node(T data)
+            {
+                this.data = data;
+            }
+
+        }
+        public Node head;
+        public void AddFront(T data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return;
+            }
+            n.next = head;
+            head.pre = n;
+            head = n;
+            return;
+        }
+        public void AddRear(T data)
+        {
+
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return;
+            }
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = n;
+            n.pre = t;
+            return;
+        }
+        public T RemoveFront()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("que is Empty");
+                return default;
+            }
+            T data = head.data;
+            head = head.next;
+            return data;
+        }
+        public T RemoveRear()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Deque is Empty");
+                return default;
+            }
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            T data = t.data;
+            t.pre.next = null;
+            return data;
+        }
+        public bool IsEmpty()
+        {
+            if (head == null)
+                return true;
+            else return false;
+        }
+        public int Size()
+        {
+            int count = 0;
+            if (head == null)
+                return count;
+            Node t = head;
+            while (t != null)
+            {
+                t = t.next;
+                count++;
+            }
+            return count;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            Node t = head;
+            while (t != null)
+            {
+                s = s + t.data + " ";
+                t = t.next;
+            }
+            return s;
+        }
+    }
+}
---
+++ Palindrome/Palindrome/PalindromeChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class PalindromeChecker
+    {
+        public static void CheckPalindrome()
+        {
+            Console.WriteLine("enter the String  to check for Palindrome:");
+            string st = Console.ReadLine();
+            isPalindrome(st);
+        }
+        public static bool isPalindrome(string st)
+        {
+            //adding elements to que
+            Deque<char> q = new Deque<char>();
+            for (int i = 0; i < st.Length; i++)
+                q.AddRear(st[i]);
+
+
+            while (!q.IsEmpty())
+            {
+                //at last there there is only 1 element will be present 
+                //so breaking at that point
+                if (q.Size() == 1)
+                    break;
+                if (!(q.RemoveFront().Equals(q.RemoveRear())))
+                {
+                    Console.WriteLine("given String is Not Palindrome");
+                    return false;
+                }
+            }
+            Console.WriteLine("Given String is Palindrome");
+            return true;
+        }
+    }
+}
